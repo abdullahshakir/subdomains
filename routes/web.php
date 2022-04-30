cc1b78d777c7820255007ee14adbbcf6f087b791
Nasar <unasar091@gmail.com>
2022-04-30 08:18:44 -0700

    adding login register auth laravel

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,7 @@ Route::domain(env('DOMAIN_TWO'))->group(function () {
         return view('example-net.index');
     });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
